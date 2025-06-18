3ee6d0b3e2e0d61e72409c0d5a11119480f1a48d
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2025-06-18 19:23:39 -0400

    Add new icon components and export them from index file

---
+++ src/components/icons/CheckmarkIcon.tsx
@@ -0,0 +1,22 @@
+import { colors } from '@/utils/colors';
+import { Icon } from '@/types/icon';
+
+export function CheckmarkIcon({ width = 18, height = 18, color }: Icon) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.5 6.91666L7.74999 11.5L6.08332 9.83333M16.7083 9C16.7083 13.2572 13.2572 16.7083 8.99999 16.7083C4.74279 16.7083 1.29166 13.2572 1.29166 9C1.29166 4.7428 4.74279 1.29166 8.99999 1.29166C13.2572 1.29166 16.7083 4.7428 16.7083 9Z"
+        stroke={color || colors.inactive}
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/components/icons/index.ts
@@ -0,0 +1,21 @@
+import { CheckmarkIcon } from './CheckmarkIcon';
+import { CopyIcon } from './CopyIcon';
+import { DuplicateIcon } from './DuplicateIcon';
+import { FileIcon } from './FileIcon';
+import { FlagIcon } from './FlagIcon';
+import { InfoIcon } from './InfoIcon';
+import { MenuIcon } from './MenuIcon';
+import { PlusIcon } from './PlusIcon';
+import { RenameIcon } from './RenameIcon';
+
+export const Icons = {
+  Checkmark: CheckmarkIcon,
+  Copy: CopyIcon,
+  Duplicate: DuplicateIcon,
+  File: FileIcon,
+  Flag: FlagIcon,
+  Info: InfoIcon,
+  Menu: MenuIcon,
+  Plus: PlusIcon,
+  Rename: RenameIcon,
+};
