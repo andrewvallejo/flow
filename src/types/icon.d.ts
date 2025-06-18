c02a8b3e3cd40d46261f224361a053624af93524
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2025-06-18 19:30:38 -0400

    Add Icon interface to define optional dimensions and color properties

---
+++ src/types/icon.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Represents an icon with optional dimensions and color
+ *
+ * @property [width] - The width of the icon in pixels
+ * @property [height] - The height of the icon in pixels
+ * @property [color] - The color of the icon
+ */
+export interface Icon {
+  width?: number;
+  height?: number;
+  color?: string;
+}
