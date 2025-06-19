c556beef5266a59957ea2edcb17f83c2b2ea0e45
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2025-06-19 01:07:53 -0400

    Refactor Home component structure for improved layout and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import NavBar from '@/components/navigation/NavBar';
 
 export default function Home() {
-  return (
-    <div>
-      <NavBar />
-      <main className="bg-[var(--color-main-background)] h-full w-full"></main>
-    </div>
-  );
+    return (
+        <div className="flex min-h-screen flex-col">
+            <main className="h-full w-full flex-1 bg-[var(--color-main-background)]"></main>
+            <footer>
+                <NavBar />
+            </footer>
+        </div>
+    );
 }
